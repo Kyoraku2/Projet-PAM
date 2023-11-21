9f749490e6200ee25f2c2e3fda851c5cd570e4e5
Almina <60519349+marie-almina@users.noreply.github.com>
2023-11-21 19:47:25 +0100

    Enumeration of all possible categories of places

--- server/src/main/java/pam/model/CategoryEnum.java
+++ server/src/main/java/pam/model/CategoryEnum.java
@@ -2,13 +2,44 @@ package pam.model;
 
 
 public enum CategoryEnum { //todo chose categories
+
+    FOOD,
     RESTAURANT,
     BAR,
     CAFE,
-    HOTEL,
-    MUSEUM,
+
+    ENTERTAINMENT,
+    CULTURE,
     PARK,
+    AMUSEMENT_PARK,
+    SPORT,
+    CINEMA,
+    THEATRE,
+    MUSEUM,
     MONUMENT,
+    RELIGIOUS_SITE,
+    HISTORICAL_SITE,
+    ZOO,
+    CASINO,
+    NIGHTCLUB,
+    LIBRARY,
+
     SHOP,
+    MALL,
+    MARKET,
+
+    HOTEL,
+
+    GAZ_STATION,
+
+    HEALTH,
+
+    WORK,
+
+    PARKING,
+    PUBLIC_TRANSPORT,
+
     OTHER
+
+
 }
